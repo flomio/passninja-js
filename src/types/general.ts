2a33242b4ff885da7d0274e9e86569f867c1304b
Tasio Victoria <tasiovictoria@gmail.com>
2020-09-20 18:11:03 -0400

    Remove axios interceptors, add code for fetching passtype keys

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,5 +1,7 @@
 export type ClientPassData = Record<string, string>;
 
+export type PassNinjaResponse = Record<string, any>;
+
 export interface SimplePassObject {
   url: string;
   serialNumber: string;
