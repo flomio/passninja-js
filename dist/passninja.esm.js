bc51cecb66b9e02bb48e1e04a3d33acd0a27a507
Tasio Victoria <tasiovictoria@gmail.com>
2020-09-20 18:37:42 -0400

    Update build

--- dist/passninja.esm.js
+++ dist/passninja.esm.js
@@ -20,24 +20,28 @@ class PassNinjaClient {
     constructor(accountId, apiKey) {
         this.#basePath = PASSNINJA_BASE_PATH;
         this.pass = {};
-        this.#initializeInterceptors = () => {
-            this.#axiosClient.interceptors.response.use(this.#handleResponse, this.#handleError);
-        };
         this.#initBindings = () => {
             this.pass.create = this.#createPass.bind(this);
             this.pass.get = this.#getPass.bind(this);
             this.pass.put = this.#putPass.bind(this);
             this.pass.delete = this.#deletePass.bind(this);
         };
-        this.#handleResponse = ({ data }) => data;
-        this.#handleError = (error) => Promise.reject(error);
         this.#extractInvalidKeys = (clientPassData) => Object.keys(clientPassData).reduce((accum, key) => {
             if (!isString(clientPassData[key])) {
                 accum[key] = clientPassData[key];
             }
             return accum;
         }, {});
-        this.#createPass = (passType, clientPassData) => {
+        this.#fetchRequiredKeysSet = async (passType) => {
+            const passTypeKeysResponse = await this.#axiosClient.get(`/passtypes/keys/${passType}`);
+            return new Set(passTypeKeysResponse.data.keys);
+        };
+        this.#extractMissingRequiredKeys = async (passType, clientPassData) => {
+            const requiredKeysSet = await this.#fetchRequiredKeysSet(passType);
+            Object.keys(clientPassData).forEach((key) => requiredKeysSet.delete(key));
+            return Array.from(requiredKeysSet);
+        };
+        this.#createPass = async (passType, clientPassData) => {
             if (!isString(passType)) {
                 throw new PassNinjaInvalidArgumentsException('Must provide passType to PassNinjaClient.createPass method. PassNinjaClient.createPass(passType: string, clientPassData: ClientPassData)');
             }
@@ -45,24 +49,32 @@ class PassNinjaClient {
             if (Object.keys(invalidKeys).length !== 0) {
                 throw new PassNinjaInvalidArgumentsException(`Invalid templateStrings provided in clientPassData object. Invalid keys: ${JSON.stringify(invalidKeys)}`);
             }
-            return this.#axiosClient
+            const missingRequiredKeys = await this.#extractMissingRequiredKeys(passType, clientPassData);
+            if (Object.keys(missingRequiredKeys).length !== 0) {
+                throw new PassNinjaInvalidArgumentsException(`Some keys that are required for this passType are missing on the provided clientPassData object. Missing keys: ${JSON.stringify(missingRequiredKeys)}`);
+            }
+            const axiosResponseData = await this.#axiosClient
                 .post('/passes', {
                 passType,
                 pass: clientPassData,
             })
-                .then((data) => ({
-                url: data.urls.landing,
-                serialNumber: data.serialNumber,
-                passType: data.passType,
-            }));
+                .then((axiosResponse) => axiosResponse.data);
+            return {
+                url: axiosResponseData.urls.landing,
+                serialNumber: axiosResponseData.serialNumber,
+                passType: axiosResponseData.passType,
+            };
         };
-        this.#getPass = (passType, serialNumber) => {
+        this.#getPass = async (passType, serialNumber) => {
             if (!isString(passType) || !isString(serialNumber)) {
                 throw new PassNinjaInvalidArgumentsException('Must provide both passType and serialNumber to PassNinjaClient.getPass method. PassNinjaClient.getPass(passType: string, serialNumber: string)');
             }
-            return this.#axiosClient.get(`/passes/${encodeURIComponent(passType)}/${encodeURIComponent(serialNumber)}`);
+            const axiosResponseData = await this.#axiosClient
+                .get(`/passes/${encodeURIComponent(passType)}/${encodeURIComponent(serialNumber)}`)
+                .then((axiosResponse) => axiosResponse.data);
+            return axiosResponseData;
         };
-        this.#putPass = (passType, serialNumber, clientPassData) => {
+        this.#putPass = async (passType, serialNumber, clientPassData) => {
             if (!isString(passType) || !isString(serialNumber)) {
                 throw new PassNinjaInvalidArgumentsException('Must provide both passType and serialNumber to PassNinjaClient.putPass method. PassNinjaClient.putPass(passType: string, serialNumber: string, clientPassData: ClientPassData)');
             }
@@ -70,18 +82,22 @@ class PassNinjaClient {
             if (Object.keys(invalidKeys).length !== 0) {
                 throw new PassNinjaInvalidArgumentsException(`Invalid templateStrings provided in clientPassData object. Invalid keys: ${JSON.stringify(invalidKeys)}`);
             }
-            return this.#axiosClient.put(`/passes/${encodeURIComponent(passType)}/${encodeURIComponent(serialNumber)}`, {
+            const axiosResponseData = this.#axiosClient
+                .put(`/passes/${encodeURIComponent(passType)}/${encodeURIComponent(serialNumber)}`, {
                 passType,
                 pass: clientPassData,
-            });
+            })
+                .then((axiosResponse) => axiosResponse.data);
+            return axiosResponseData;
         };
-        this.#deletePass = (passType, serialNumber) => {
+        this.#deletePass = async (passType, serialNumber) => {
             if (!isString(passType) || !isString(serialNumber)) {
                 throw new PassNinjaInvalidArgumentsException('Must provide both passType and serialNumber to PassNinjaClient.deletePass method. PassNinjaClient.deletePass(passType: string, serialNumber: string)');
             }
-            return this.#axiosClient
+            const axiosResponseData = this.#axiosClient
                 .delete(`/passes/${encodeURIComponent(passType)}/${encodeURIComponent(serialNumber)}`)
                 .then(() => serialNumber);
+            return axiosResponseData;
         };
         if (!isString(accountId) || !isString(apiKey)) {
             throw new PassNinjaInvalidArgumentsException('Must provide both accountId and apiKey to PassNinjaClient constructor. PassNinjaClient(accountId: string, apiKey: string)');
@@ -94,16 +110,14 @@ class PassNinjaClient {
                 'x-api-key': apiKey,
             },
         });
-        this.#initializeInterceptors();
         this.#initBindings();
     }
     #basePath;
     #axiosClient;
-    #initializeInterceptors;
     #initBindings;
-    #handleResponse;
-    #handleError;
     #extractInvalidKeys;
+    #fetchRequiredKeysSet;
+    #extractMissingRequiredKeys;
     #createPass;
     #getPass;
     #putPass;
